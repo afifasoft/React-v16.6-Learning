7f5296a5e8f02fb2a4f4d46a0c255431de20c250
afifasoft <abdul.pesit@gmail.com>
2019-08-09 22:54:59 +0400

    component overview

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getButtonText() {
+  return 'Click on me!';
+}
+
+
+function getTime() {
+  return (new Date()).toLocaleTimeString('en-US');
+}
+// Create a react component
+const App = () => {
+//  const buttonText = 'Click Me!';
+//  const buttonText = '1234567';
+//  const buttonText = ['Hi', 'There'];
+//  const buttonText = [10, 20];
+    const buttonText = {text: 'Click Me!'};
+    const labelText = 'Enter name:'
+  return (
+    <div>
+      <label className="label" htmlFor="name">{labelText}</label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor:'blue', color:'white'}}>{buttonText.text}</button>
+      <h3>{getTime()}</h3>
+    </div>
+  );
+ };
+
+// Take the react component and show it on the screen
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
